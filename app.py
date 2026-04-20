c60ff07c55a129b889400262aa04a392ccfd2078
nithish1923 <63442177+nithish1923@users.noreply.github.com>
2026-04-20 16:37:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
 from services.pdf_parser import extract_text
 from db.vectordb import store_policy
