c426c80886d8a7d5af592166dc3964ad1c43c134
nithish1923 <63442177+nithish1923@users.noreply.github.com>
2026-04-20 17:17:08 +0530

    Update rag.py

--- services/rag.py
+++ services/rag.py
@@ -1,6 +1,5 @@
 from db.vectordb import retrieve_policy
 
 def get_policy_context(query):
-    # 🔥 Include identity + rules in search
-    enhanced_query = "insurance policy holder name insured details coverage claim rules " + query
+    enhanced_query = "insured name policy holder details coverage claim " + query
     return retrieve_policy(enhanced_query)
